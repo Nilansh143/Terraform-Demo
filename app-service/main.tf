372351ce72ec7c62660148d88bced691ef7b6d2c
ratnac37 <61957578+ratnac37@users.noreply.github.com>
2020-04-10 16:18:15 +0530

    done

--- app-service/main.tf
+++ app-service/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "test" {
   name     = "terraform-resource-group"
-  location = "West Europe"
+  location = "East US"
 }
 
 resource "azurerm_app_service_plan" "test" {
   name                = "terraform-appserviceplan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "azurerm_resource_group.test.location"
+  resource_group_name = "azurerm_resource_group.test.name"
 
   sku {
     tier = "Standard"
@@ -16,9 +16,9 @@ resource "azurerm_app_service_plan" "test" {
 
 resource "azurerm_app_service" "test" {
   name                = "terraform-app-service"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+  location            = "azurerm_resource_group.test.location"
+  resource_group_name = "azurerm_resource_group.test.name"
+  app_service_plan_id = "azurerm_app_service_plan.test.id"
 
   site_config {
     dotnet_framework_version = "v4.0"
